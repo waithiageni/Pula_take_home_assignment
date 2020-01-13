a7988af33856105803d98e672cf7260cb0d0341a
Waithiageni <mwangiwaithiageni33@gmail.com>
2020-01-13 18:21:48 +0300

    commit question1 update

--- Question 1/question1.py
+++ Question 1/question1.py
@@ -1,26 +1,11 @@
-# import datetime
-
 def calculate_age():
-
 	name = input("Enter your name :")
-	age = int(input("Enter your age :"))
-	current_year = int(input("year :"))
-	# now = datetime.datetime.now()
-	print (name, "in" + current_year + "your age is" + age)
+	age = input("Enter your age :")
+	Y = 2016
+	# Current_year = Y
 
-# class Calculator:
-# 	def __init__(self,name,current_year,current_age):
-# 		self.name = name
-# 		self.current_year = current_year
-# 		self.current_age = current_age
+	for x in range(49,100,7):
+		Y+=7
+		print ( name + "," + " in " + str(Y) + " "+ "your age is " + str(x))
 
-# 	def calculate_age(self):
-# 		name = input("Enter your name: ")
-# 		print ("My name is " + name)	
-
-# import datetime
-
-# name = input ("Enter your name :") 
-# age = input("Enter your age :")
-# now = datetime.datetime.now()
 
